536e5e5650d786ca42c9641c5e55aa43ceaa690b
Jei-sKappa <guzzojacopo95@gmail.com>
2026-02-18 09:37:06 +0100

    feat(generator): add comments to ingnore lint and stop format

--- example/lib/presentation/todo/bloc/todo_bloc.bes.g.dart
+++ example/lib/presentation/todo/bloc/todo_bloc.bes.g.dart
@@ -1,10 +1,15 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// dart format off
 
 part of 'todo_bloc.dart';
 
 // **************************************************************************
 // BlocEventStatusGenerator
 // **************************************************************************
+
+// dart format off
 
 extension $TodoBlocEmitterX on Emitter<TodoState> {
   void _emitEventStatus<T extends TodoEvent>(
@@ -22,23 +27,11 @@ extension $TodoBlocEmitterX on Emitter<TodoState> {
   void loading<T extends TodoEvent>(T event, TodoState state) =>
       _emitEventStatus(event, const LoadingEventStatus(), state);
 
-  void success<TData extends dynamic, T extends TodoEvent>(
-          T event, TodoState state,
-          [TData? data]) =>
-      _emitEventStatus(
-          event,
-          SuccessEventStatus<TData>(
-            data,
-          ),
-          state);
+  void success<TData extends dynamic, T extends TodoEvent>(T event, TodoState state, [TData? data]) =>
+      _emitEventStatus(event, SuccessEventStatus<TData>(data, ), state);
 
-  void failure<TFailure extends Exception, T extends TodoEvent>(
-          T event, TodoState state,
-          [TFailure? error]) =>
-      _emitEventStatus(
-          event,
-          FailureEventStatus<TFailure>(
-            error,
-          ),
-          state);
+  void failure<TFailure extends Exception, T extends TodoEvent>(T event, TodoState state, [TFailure? error]) =>
+      _emitEventStatus(event, FailureEventStatus<TFailure>(error, ), state);
 }
+
+// dart format on

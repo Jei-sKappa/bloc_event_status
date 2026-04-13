935a861490cacc5febf633d71ee7312e0f3739e2
Jei-sKappa <guzzojacopo95@gmail.com>
2026-04-13 17:07:44 +0200

    refactor(example_bes): reformat generator output

--- packages/bloc_event_status/example/lib/main.bes.g.dart
+++ packages/bloc_event_status/example/lib/main.bes.g.dart
@@ -27,24 +27,11 @@ extension $TodoBlocEmitterX on Emitter<TodoState> {
   void loading<T extends TodoEvent>(T event, TodoState state) =>
       _emitEventStatus(event, const LoadingTodoEventStatus(), state);
 
-  void success<TData extends Object?, T extends TodoEvent>(
-          T event, TodoState state,
-          [TData? data]) =>
-      _emitEventStatus(
-          event,
-          SuccessTodoEventStatus<TData>(
-            data,
-          ),
-          state);
+  void success<TData extends Object?, T extends TodoEvent>(T event, TodoState state, [TData? data]) =>
+      _emitEventStatus(event, SuccessTodoEventStatus<TData>(data, ), state);
 
-  void failure<T extends TodoEvent>(
-          T event, TodoState state, Exception error) =>
-      _emitEventStatus(
-          event,
-          FailureTodoEventStatus(
-            error,
-          ),
-          state);
+  void failure<T extends TodoEvent>(T event, TodoState state, Exception error) =>
+      _emitEventStatus(event, FailureTodoEventStatus(error, ), state);
 }
 
 // dart format on
